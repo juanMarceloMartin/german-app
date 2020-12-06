7fef3981db19b3dfcf7d18b22cf7298de7103be8
Marce <jmarce_83@hotmail.com>
2020-12-06 01:11:51 -0300

    backend added

---
+++ server/server.ts
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+    if('OPTIONS' === req.method) {
+        res.sendStatus(200);
+    } else {
+        console.log(`${req.ip} ${req.method} ${req.url}`);
+        next();
+    }
+});
+
+const PORT = process.env.PORT || 4201;
+
+app.listen(PORT, () => console.log('server running'));
